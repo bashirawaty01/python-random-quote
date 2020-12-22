73146fddfa593d7de97f21d8d367416e05709aef
Bashir Awaty <bawaty@outlook.com>
2020-12-22 23:31:50 +1100

    "Replaced the quotes in .txt"

--- get-quote.py
+++ get-quote.py
@@ -8,13 +8,20 @@ def primary():
     quotes = f.readlines()
     f.close()
 
-    last = 13
+    last = len(quotes) - 1
 
     # If you want to add or remove quotes from your text file, you could change the last variable to update automatically:
     # last = len(quotes) - 1
 
     rnd = random.randint(0, last)  # 0 is the lowest-possible number
+
     print(quotes[rnd])
+
+    # print out more than one quote
+    # rnd = random.randint(0, last)  # 0 is the lowest-possible number
+    # rnd1 = random.randint(0, last)  # 0 is the lowest-possible number
+    # print(quotes[rnd])
+    # print(quotes[rnd1])
 
 
 if __name__ == "__main__":
